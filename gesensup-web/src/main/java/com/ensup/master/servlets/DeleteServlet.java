c1ad9f6df7ecd0bf6644ccfefba31f6e9e41ef92
Braham Moussouni <braham.moussouni@etudiant.univ-lr.fr>
2020-07-07 11:27:04 +0200

    servlet

---
+++ gesensup-web/src/main/java/com/ensup/master/servlets/DeleteServlet.java
@@ -0,0 +1,50 @@
+// Classe delete servlet du projet Formulaire Java JSP Servlet
+package com.objis.gestionformationssession.presentation.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.objis.gestionformationssession.service.ServiceGestion;
+
+/**
+ * Servlet implementation class DeleteServlet
+ */
+@WebServlet("/DeleteServlet")
+public class DeleteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DeleteServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String email = request.getParameter("email");
+		RequestDispatcher dispatcher;
+		ServiceGestion serviceGestion = new ServiceGestion();
+		serviceGestion.delete(email);
+		dispatcher = request.getRequestDispatcher("accueil.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
